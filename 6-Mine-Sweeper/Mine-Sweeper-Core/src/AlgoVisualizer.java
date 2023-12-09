6018258daed99beca7b3d1217d9a5f2cfe5110f6
Universe-E <zehuakong.uranus@gmail.com>
2023-12-10 00:16:01 +1100

    add 8 visualized models

---
+++ 6-Mine-Sweeper/Mine-Sweeper-Core/src/AlgoVisualizer.java
@@ -0,0 +1,90 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class AlgoVisualizer {
+
+    private MineSweeperData data;
+    private AlgoFrame frame;
+
+    public AlgoVisualizer(int N, int M, int mineNumber){
+
+        data = new MineSweeperData(N, M, mineNumber);
+        int blockSide = 32;
+        int sceneWidth = M * blockSide;
+        int sceneHeight = N * blockSide;
+
+        EventQueue.invokeLater(() -> {
+            frame = new AlgoFrame("Mine Sweeper", sceneWidth,sceneHeight);
+            frame.addMouseListener(new AlgoMouseListener());
+            new Thread(this::run).start();
+        });
+    }
+
+    public void run(){
+
+        this.setData(false, -1, -1);
+
+    }
+
+    private void setData(boolean isLeftClicked, int x, int y){
+        if(isLeftClicked){
+            if(data.inArea(x, y))
+                if(data.isMine(x, y)){
+                    // Game Over
+                    data.open[x][y] = true;
+                }
+                else
+                    data.open(x, y);
+        }
+        else{
+            if(data.inArea(x, y))
+                // data.flags[x][y] = true;
+                data.flags[x][y] = !data.flags[x][y];
+        }
+
+        frame.render(data);
+        int DELAY = 5;
+        AlgoVisHelper.pause(DELAY);
+    }
+
+    public void addAlgoMouseListener(){
+        frame.addMouseListener(new AlgoMouseListener());
+    }
+
+    private class AlgoMouseListener extends MouseAdapter{
+
+        @Override
+        public void mouseReleased(MouseEvent event){
+
+            event.translatePoint(
+                    -(int)(frame.getBounds().width - frame.getCanvasWidth()),
+                    -(int)(frame.getBounds().height - frame.getCanvasHeight())
+            );
+
+            Point pos = event.getPoint();
+
+            int w = frame.getCanvasWidth() / data.M();
+            int h = frame.getCanvasHeight() / data.N();
+
+            int x = pos.y / h;
+            int y = pos.x / w;
+            // System.out.println(x + " , " + y);
+
+            if(SwingUtilities.isLeftMouseButton(event))
+                setData(true, x, y);
+            else if(SwingUtilities.isRightMouseButton(event))
+                setData(false, x, y);
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int N = 20;
+        int M = 20;
+        int mineNumber = 20;
+
+        new AlgoVisualizer(N, M, mineNumber);
+    }
+}
